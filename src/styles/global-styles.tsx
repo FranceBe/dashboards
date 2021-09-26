5fe152821279ec33530d36f6d5190b9e12199964
France Bérut <france.berut@gmail.com>
2021-09-26 11:27:31 +0200

    Add a variables file and improve global style + add it in storybook conf

--- src/styles/global-styles.tsx
+++ src/styles/global-styles.tsx
@@ -1,7 +1,10 @@
 import { createGlobalStyle } from 'styled-components'
+import { palette } from 'styles/variables'
 
 export const GlobalStyle = createGlobalStyle`
   body {
     margin: 1em;
+    background-color: ${palette.blue_light};
+    color: ${palette.grey_primary};
   }
 `
